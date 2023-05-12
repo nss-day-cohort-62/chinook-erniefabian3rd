fe33aa5e07babce81bb705ccbe27d29fd828cef4
Ernie Fabian <erniefabian3rd@gmail.com>
2023-05-12 11:32:03 -0500

    Top Sales Agent: SOLVED

---
+++ top_agent.sql
@@ -0,0 +1,11 @@
+SELECT
+e.FirstName || ' ' || e.LastName AS FullName,
+ROUND(SUM(i.Total),2) AS TotalSales
+FROM Employee e
+JOIN Customer c
+    ON e.EmployeeId = c.SupportRepId
+JOIN Invoice i
+    ON c.CustomerId = i.CustomerId
+GROUP BY e.EmployeeId
+ORDER BY TotalSales DESC
+LIMIT 1
