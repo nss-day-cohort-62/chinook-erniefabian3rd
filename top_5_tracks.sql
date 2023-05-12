3477676f8f1046b62ece33819cf065d65520215f
Ernie Fabian <erniefabian3rd@gmail.com>
2023-05-12 11:58:57 -0500

    Top 5 Tracks

---
+++ top_5_tracks.sql
@@ -0,0 +1,11 @@
+SELECT
+COUNT(il.InvoiceId) AS TrackCount,
+t.Name AS TrackName
+FROM InvoiceLine il
+JOIN Track t
+    ON t.TrackId = il.TrackId
+JOIN Invoice i
+    ON i.InvoiceId = il.InvoiceId
+GROUP BY il.InvoiceId
+ORDER BY TrackCount DESC
+LIMIT 5
